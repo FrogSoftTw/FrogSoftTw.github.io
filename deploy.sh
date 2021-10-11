aa828e31847e12cbdc8e79529296eb27b3be6a7e
Jack Chen <tonyjohn7@hotmail.com.tw>
2021-10-11 13:46:27 +0800

    Update deploy.sh
    Signed-off-by: Jack Chen <tonyjohn7@hotmail.com.tw>

--- deploy.sh
+++ deploy.sh
@@ -11,5 +11,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f https://github.com/FrogSoftTw/FrogSoftTw.github.io.git main:deploy
+git push -f https://github.com/FrogSoftTw/FrogSoftTw.github.io.git master:deploy
 cd ../
